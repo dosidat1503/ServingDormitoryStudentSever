7d669a530cc9b8ae8427c89a656a18d62a21a63a
Mỹ Xoan Trần Thị <xoanxoan2501>
2024-07-03 20:21:28 +0700

    account

--- app/Http/Controllers/AccountManagementController.php
+++ app/Http/Controllers/AccountManagementController.php
@@ -1,158 +1,130 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Auth\Events\Verified;
-use App\Models\User;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use stdClass;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail; // Add this line
+use Illuminate\Support\Str;
 
-class AuthenticationController extends Controller
+class AccountManagementController extends Controller
 {
-    public function signup(Request $request) {
-        $Validator = Validator::make($request->all(), [ 
-            'email' => 'required|email:191|unique:user', 
-        ]);
+    public function getInfoAccount(Request $request){
+        $user = $request->userID;
 
-        if($Validator->fails()){
-            return response()->json([
-                'statusCode' =>  422,
-            ]);
-        } 
-        
-        $user =  User::create([
-            'name' => $request->fullname,
-            'email' => $request->email, 
-            'phone' => $request->phoneNumber, 
-            'password' => Hash::make($request->password),
-            'birthday' => $request->birthday,
-            'gender' => $request->gender,
-        ]);
-
-        $user->sendEmailVerificationNotification(); 
+        $accountInfo = DB::table('USER')
+                        ->join('IMAGE', 'USER.AVT_IMAGE_ID', '=', 'IMAGE.IMAGE_ID')
+                        ->select( 'NAME', 'EMAIL', 'PHONE', 'BIRTHDAY', 'LINK_FB', 'SCHOOL', 'ADDRESS', 'URL')
+                        ->get();
 
         return response()->json([
-            'statusCode' =>  200,
+            'statusCode' => 200,
+            'accountInfo' => $accountInfo[0],
         ]);
     }
- 
-    public function signin(Request $request){   
+
+    public function verifyChangeMail(Request $request){
+        $userID = $request->userID; 
         $email = $request->email;
-        $password = $request->password;
-        $taikhoan = User::where('email', $request->email)->first();
 
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email:191',
-            'password' => 'required',
-        ]);
-        
-        if($validator->fails())
-        {
-            return response()->json([
-                'statusCode' => 1,// không đúng định dạng
-                'message' => "Mật khẩu hoặc email không đúng", 
-            ]);
-        }
-        else
-        {
-            if(!$taikhoan) {  
-                return response()->json([
-                    'statusCode' => 1,
-                    'message' => "Email không tồn tại",
-                ]);  
-            }
-            else if($taikhoan && !Hash::check($request->password,$taikhoan->PASSWORD)){ 
-                return response()->json([
-                    'statusCode'=>3,
-                    'message' => "Mật khẩu sai",
-                ]);
-            }
-            else { 
-                if (is_null($taikhoan->email_verified_at)) {  
-                    return response()->json([
-                        'statusCode'=>4,
-                        'message' => "Tài khoản chưa được xác nhận",
-                    ]);
-                }   
-                $token = $taikhoan->createToken($taikhoan->email.'_Token')->plainTextToken;
-
-
-                $infoUserAtHome =  DB::table("user")
-                ->join("image", "image.IMAGE_ID", "=", "user.AVT_IMAGE_ID")
-                ->where("user.USER_ID", $taikhoan->USER_ID)
-                ->select("user.NAME", "image.URL as AVT_URL")
-                ->first();
-                    
-                return response()->json([
-                    'statusCode' => 200,
-                    'token' => $token,
-                    'userID' => $taikhoan->USER_ID,
-                    'infoUserAtHome' => $infoUserAtHome
-                ]);
-                // }
-            } 
-        } 
-    }
-
-    public function verify(Request $request) {
-        // Tìm người dùng có ID tương ứng trong request
-        $matk = $request->route('id');
-        $user = User::where('USER_ID', $matk)->firstOrFail(); 
-        $email = User::where('USER_ID', $matk)->value('EMAIL'); 
-
-        // Kiểm tra xem hash truyền vào có khớp với hash của người dùng không
-        if (! hash_equals((string) $request->route('hash'), sha1($email))) {
-            return response()->json(['message' => 'Invalid verification link'], 400);
-        }
-
-        // Kiểm tra xem người dùng đã xác nhận email trước đó chưa
-        if ($user->hasVerifiedEmail()) {
-            return response()->json(['message' => 'Email already verified'], 200);
-        }
-        // dd($user->markEmailAsVerified(), $user->hasVerifiedEmail(), $user, $matk);
-
-        // Xác nhận email cho người dùng và gửi event Verified
-        if ($user->markEmailAsVerified()) {
-            event(new Verified($user));
-        }
-
-        return response()->json(['message' => 'Email verified'], 200);
-    }
-
-    public function sendMailRecoverPassword(Request $request) {
-        $email = $request->email;
-        $taikhoan = User::where('email', $request->email)->first();
-        if (!$taikhoan) { 
-            $data = new stdClass();
-            $data->email = "Email không tồn tại"; 
-            return response()->json([
-                'statusCode'=> 1,
-                'message' => "Email không tồn tại",
-            ]);  
-        }
-        
-        $password = Str::random(10);
-        $password_hash = Hash::make($password);
-        DB::update("UPDATE user SET PASSWORD = '$password_hash' Where EMAIL = '$email'");
-        Mail::send('mailRecoverPassword', ['password' => $password], function($message) use ($email){
+        $code = Str::random(5); 
+        DB::update("UPDATE user SET CODEVERIFYCHANGEMAIL = '$code' Where USER_ID = '$userID'");
+        Mail::send('mailSendCodeToChangeMail', ['code' => $code], function($message) use ($email){
             $message->to($email);
         });
+
+        DB::table('USER')
+            ->where('USER_ID', $userID)
+            ->update([
+                'CODEVERIFYCHANGEMAIL' => $code,
+            ]);
+        
         return response()->json([
-            'statusCode'=>200,  
-        ]);  
+            'statusCode' => 200,
+        ]);
     }
 
-    public function logout() { 
-        Auth::user()->tokens()->delete();
+    public function updateAccountInfo(Request $request){ 
+        $userID = $request->userID;
+        $name = $request->name;
+        $phoneNumber = $request->phoneNumber;
+        $avatar = $request->avatar;
+        $email = $request->email;
+        $birthDate = $request->birthDate;
+        $facebookLink = $request->facebookLink;
+        $schoolName = $request->schoolName;
+        $address = $request->address;
+        $avatarURLBeforeChange = $request->avatarURLBeforeChange;
+        $codeVerifyEmail = $request->codeVerifyEmail;
+        $emailBeforeChange = $request->emailBeforeChange;
+
+        if($emailBeforeChange != $email){
+            $user = DB::table('USER')
+                    ->where('USER_ID', $userID)
+                    ->select('CODEVERIFYCHANGEMAIL')->get();
+            
+            if($user[0]->CODEVERIFYCHANGEMAIL != $codeVerifyEmail){
+                return response()->json([
+                    'statusCode' => 1,
+                    'message' => 'Mã xác nhận không đúng',
+                ]);
+            } 
+        }
+        $imageID = 0;
+        $updateData = [
+            'NAME' => $name,
+            'PHONE' => $phoneNumber,
+            'EMAIL' => $email,
+            'BIRTHDAY' => $birthDate,
+            'LINK_FB' => $facebookLink,
+            'SCHOOL' => $schoolName,
+            'ADDRESS' => $address,
+            'CODEVERIFYCHANGEMAIL' => ""
+        ];
+        if($avatarURLBeforeChange !== $avatar){
+            $imageID = DB::table('IMAGE')
+                        ->insertGetId([
+                            'URL' => $avatar,
+                        ]);  
+            $updateData['AVT_IMAGE_ID'] = $imageID;
+        }
+        $user = DB::table('USER')
+            ->where('USER_ID', $userID)
+            ->update($updateData);
+        
         return response()->json([
-            'statusCode'=>200,
-            'message'=>'Logged out Successfully',
+            'statusCode' => 200,
+            'message' => 'Cập nhật thông tin thành công',
+        ]);
+        
+    }
+
+    public function changePassword(Request $request){
+        $userID = $request->userID;
+        $oldPassword = $request->oldPassword;
+        $newPassword = $request->newPassword;
+
+        $user = DB::table('USER')
+                ->where('USER_ID', $userID)
+                ->select('PASSWORD')->get();
+        
+        if(!Hash::check($oldPassword, $user[0]->PASSWORD)){
+            return response()->json([
+                'statusCode' => 1,
+                'message' => 'Mật khẩu cũ không đúng',
+            ]);
+        }
+
+        DB::table('USER')
+            ->where('USER_ID', $userID)
+            ->update([
+                'PASSWORD' => Hash::make($newPassword),
+            ]);
+
+        return response()->json([
+            'statusCode' => 200,
+            'message' => 'Đổi mật khẩu thành công',
         ]);
     }
 }
